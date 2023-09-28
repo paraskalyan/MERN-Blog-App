5d45827ef329ff470f3e8fa033c089d1a7fb0c72
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-09-28 17:32:31 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/logo-zingy.png'
 import SearchIcon from '@mui/icons-material/Search';
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import { Link, Navigate, useNavigate } from "react-router-dom";
-const Navbar = ({isAuth, setAuth}) => {
+import { useDispatch, useSelector } from 'react-redux';
+import { changeAuth } from '../features/authSlice';
+import { setSearchQuery } from '../features/searchQuery';
+const Navbar = () => {
+    const blogs = useSelector(state=> state.blogs.blogs)
+    console.log(blogs)
+    const isAuth = useSelector(state=> state.auth.isAuth)
+    const dispatch = useDispatch();
     const navigate = useNavigate();
+    const handleSearchChange = (e)=>{
+        dispatch(setSearchQuery(e.target.value))
+
+    }
     const handleLogout = ()=>{
         localStorage.clear()
-        setAuth(false)
+        dispatch(changeAuth(false))
     } 
     
     const openProfile = ()=>{
@@ -18,7 +29,7 @@ const Navbar = ({isAuth, setAuth}) => {
             <div className='h-[7vh] fixed top-0 bg-white w-full flex items-center justify-between p-5 shadow-md '>
             <div className='flex gap-9 items-center'>
                 <div className=''><Link to='/'><img src={logo} height="80px" width="80px"/></Link></div>
-                <div className='border border-[#f0f0f0] px-2'><input className='outline-none text-[14px]' type='text'/><SearchIcon style={{fontSize: '18px'}}/></div>
+                <div className='border border-[#f0f0f0] px-2'><input className='outline-none text-[14px]' onChange={handleSearchChange} type='text'/><SearchIcon style={{fontSize: '18px'}}/></div>
             </div>
             <div className='flex gap-6 items-center text-[15px]'>
                 <div><EditNoteIcon/><Link to='/write'>Write</Link></div>
@@ -36,7 +47,7 @@ const Navbar = ({isAuth, setAuth}) => {
         <div className='h-[7vh] fixed top-0 bg-white w-full flex items-center justify-between p-5 shadow-md '>
             <div className='flex gap-9 items-center'>
                 <div className=''><Link to='/'><img src={logo} height="80px" width="80px"/></Link></div>
-                <div className='border border-[#f0f0f0] px-2'><input className='outline-none text-[14px]' type='text'/><SearchIcon style={{fontSize: '18px'}}/></div>
+                <div className='border border-[#f0f0f0] px-2'><input className='outline-none text-[14px]' onChange={handleSearchChange} type='text'/><SearchIcon style={{fontSize: '18px'}}/></div>
             </div>
             <div className='flex gap-6 items-center text-[15px]'>
                 <div><EditNoteIcon/><Link to='/login'>Write</Link></div>
