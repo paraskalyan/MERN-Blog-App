226a496bcff5c60190b42c80c8161df630a15b73
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-09-28 17:32:10 +0530

    Update Blogs.jsx

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -2,23 +2,28 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Blog from './Blog'
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToBlogs } from '../features/blogSlice';
 
 const Blogs = () => {
+    const dispatch = useDispatch()
+    const searchQuery = useSelector(state=> state.search.searchQuery)
+    console.log(searchQuery)
     const [ data, setData ] = useState(null);
     const [ updated, setUpdated ] = useState();
     function getData(){
         axios.get('http://localhost:4000/blog')
         .then(response=>{
-            console.log(response.data)
+            dispatch(addToBlogs(response.data))
             setData(response.data)
         })
         .catch(error=>console.log(error))
     }
 
-    useEffect(()=>{
+  
+     useEffect(()=>{
         getData();
     },[])
-   
 
     const handleDelete = (deletedId) => {
         // Update the state to remove the deleted blog
@@ -36,7 +41,7 @@ const Blogs = () => {
             {data && data.map(blog=>{
                 const blogUrl = `/blog/${blog._id}`
                 return(
-                    <Link to={blogUrl}>
+                    <Link to={blogUrl} key={blog._id}>
                         <Blog blog={blog} onDelete={handleDelete} onUpdate={handleUpdate}/>
                     </Link>
                     )
