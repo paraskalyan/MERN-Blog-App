49561e9e92363ad9080786c52ae99d78c37e7e6b
Paras Kalyan <98208059+paraskalyan@users.noreply.github.com>
2023-09-30 17:49:30 +0530

    Add files via upload

---
+++ frontend/src/Pages/UserFollowers.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const UserFollowers = () => {
+    const user = useSelector(state => state.user.user)
+    console.log(user.following)
+    // var followers = []
+    // const fetchUser =async (id)=> {
+    //     await axios.get(`http://localhost:4000/user/${id}`)
+    //     .then(res=>{
+    //         followers.push(res.data)
+    //     })
+    //     .catch(err=> console.log(err))
+
+    // }
+    // for (var id in user.followers){
+    //     fetchUser(user.followers[id])
+    // }
+    // console.log(followers)
+
+    return (
+        <div className='mt-[7vh] mx-[30%] py-[40px] flex flex-col items-center bg-[#FAFAFA] h-[93vh] shadow-md'>
+            {user.followers.length === 0 && <h1 className='text-[2rem]'>No one is following you!</h1>}
+            {user.followers.map(f => {
+                return (
+                    <div className='border-b border-t w-full px-2 h-[50px] flex items-center justify-between  cursor-pointer hover:bg-slate-200'>
+                        <Link to={`/user/${f}`}>
+                            <div className='flex items-center gap-2'>
+                                <div className='h-[40px] w-[40px] border border-black rounded-full'></div>
+                                <p>{f}</p>
+                            </div>
+                        </Link>
+                        <div>
+
+                        {
+                            user.following.map(fo => {
+                                return fo === f ? <button className=''>Unfollow</button> : <button className=''>Follow</button>
+                            })
+                        }
+                        </div>
+                    </div>
+    )
+})}
+
+        </div >
+    )
+}
+
+export default UserFollowers
